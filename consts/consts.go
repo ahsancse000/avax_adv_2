9ebacf99576a241bbd5bafbf00f42d8ce3db98cd
ahsancse000 <165180664+ahsancse000@users.noreply.github.com>
2024-08-11 14:10:53 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "cse"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "ahsan"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "feb24"
 )
 
 var ID ids.ID
